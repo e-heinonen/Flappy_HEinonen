94bd0cc8c6a3706ac6a88abd0c7b658f4cbfedf1
e-heinonen <71631097+e-heinonen@users.noreply.github.com>
2021-01-12 10:59:25 +0200

    Flappyn filet

---
+++ Flappy Kopio Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gets player's rigidbody
+        playerRb = GetComponent<Rigidbody>();
+        // Gets physics for the gravity
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player jumps when space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
