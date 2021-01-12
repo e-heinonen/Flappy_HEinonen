573a263a3c50b7ac1ee02b41443f18f53cc29100
GitHubmaster420 <joonavjs7@hotmail.fi>
2021-01-12 19:33:58 +0000

    Pipe Spawner & Death

---
+++ Flappy Kopio Unity/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+    }
+}
