c8d7ea20353a68fa409e61a5e21a0f8b431623b0
e-heinonen <71631097+e-heinonen@users.noreply.github.com>
2021-01-19 11:35:24 +0200

    Spawneri hoidossa, parempi ulkoasu tms. 19.01.2021

---
+++ Flappy Kopio Unity/Assets/Scripts/DieWhenHitPipe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DieWhenHitPipe : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Object.Destroy(gameObject);
+        // Loads the game from the beginning
+        SceneManager.LoadScene(0);
+    }
+}
