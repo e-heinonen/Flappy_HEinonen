2c859601d1bcf2e41dafdefd46aaf7825b22ddf5
GitHubmaster420 <joonavjs7@hotmail.fi>
2021-02-17 00:46:47 +0000

    Ready?

---
+++ Flappy Kopio Unity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+    public Button switchCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Does the playgame function when clicking play button
+        playButton.onClick.AddListener(playgame);
+        // Closes software when clicking quit button
+        quitButton.onClick.AddListener(Application.Quit);
+
+    }
+
+    // Loads level
+    void playgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
--- Flappy Kopio Unity/Assets/Scripts/PlayerController.cs
+++ Flappy Kopio Unity/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public UIScript playerUI;
     public float jumpForce;
     public float gravityModifier;
     public float movementSpeed;
@@ -14,6 +16,9 @@ public class PlayerController : MonoBehaviour
     {
         // Gets player's rigidbody
         playerRb = GetComponent<Rigidbody>();
+
+        // Resets gravity
+        Physics.gravity = new Vector3(0, -9.8F, 0);
         // Gets physics for the gravity
         Physics.gravity *= gravityModifier;
     }
@@ -22,12 +27,30 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move forward at a fixed speed
-        transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        // transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
 
-        // Player jumps when space is pressed
+        // Player first stops any momentum it had and jumps when space is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            playerRb.velocity = Vector3.zero;
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if object's layer is set to "Enemy" (Ground and the pipes have this layer):
+        if (other.gameObject.layer == 8)
+        {
+            // Loads the game from the main menu
+            SceneManager.LoadScene(0);
+        }
+
+        // If object's layer is set to "Point" (the space between the pipes has that layer so it detects when player has moved through the pipes):
+        if (other.gameObject.layer == 9)
+        {
+            playerUI.pointsUpdate();
+            Debug.Log("You have " + playerUI.points + " points!");
         }
     }
 }
