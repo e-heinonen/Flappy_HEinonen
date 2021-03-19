891d8aa01c9ee67f324e16cba4f59276b8b61fc4
e-heinonen <71631097+e-heinonen@users.noreply.github.com>
2021-03-19 09:24:02 +0200

    background models added
    background models added and they move and spawn

---
+++ Flappy Kopio Unity/Assets/Scripts/BGObjectMoveLeft.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGObjectMoveLeft : MonoBehaviour
+{
+    public float objSpeed;
+    public float objHeight;
+    public float objY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * objSpeed);
+        // Move the pipes back in front of player if they've gone too far
+        if (transform.position.x < -20)
+        {
+            transform.position = new Vector3(15, objY, objHeight);
+        }
+    }
+}
